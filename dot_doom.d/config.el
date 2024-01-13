f3c2eff6bf01f6caaa247f8f00742e3f8fbc4596
S-Q-U-E-K <zeke@aylin.net>
2024-01-13 04:09:20 +0000

    doom config

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -113,3 +113,28 @@
   :after (ewal-evil-cursors winum)
   :init (setq powerline-default-separator nil)
   :config (spaceline-doom-theme))
+
+(defun chezmoi--evil-insert-state-enter ()
+  "Run after evil-insert-state-entry."
+  (chezmoi-template-buffer-display nil (point))
+  (remove-hook 'after-change-functions #'chezmoi-template--after-change 1))
+
+(defun chezmoi--evil-insert-state-exit ()
+  "Run after evil-insert-state-exit."
+  (chezmoi-template-buffer-display nil)
+  (chezmoi-template-buffer-display t)
+  (add-hook 'after-change-functions #'chezmoi-template--after-change nil 1))
+
+(use-package chezmoi)
+(defun chezmoi-evil ()
+  (if chezmoi-mode
+      (progn
+        (add-hook 'evil-insert-state-entry-hook #'chezmoi--evil-insert-state-enter nil 1)
+        (add-hook 'evil-insert-state-exit-hook #'chezmoi--evil-insert-state-exit nil 1))
+    (progn
+      (remove-hook 'evil-insert-state-entry-hook #'chezmoi--evil-insert-state-enter 1)
+      (remove-hook 'evil-insert-state-exit-hook #'chezmoi--evil-insert-state-exit 1))))
+(add-hook 'chezmoi-mode-hook #'chezmoi-evil)
+(global-set-key (kbd "C-c C f")  #'chezmoi-find)
+(global-set-key (kbd "C-c C s")  #'chezmoi-write)
+
