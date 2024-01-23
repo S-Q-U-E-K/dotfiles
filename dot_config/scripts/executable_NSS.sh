4cdb4934bacc4ddceeed2c9c8580cc036a6deecb
Zeke Aylin <zeke@aylin.net>
2024-01-23 09:08:39 +0000

    misc

--- dot_config/scripts/executable_NSS.sh
+++ dot_config/scripts/executable_NSS.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
 read -p "Which Year:" year
+sleep 0.1
 read -p "Which Semester:" semester
+sleep 0.1
 read -p "Which Subject:" subject
+sleep 0.1
 read -p "Name of screenshot:" filename
+sleep 0.1
 grim -g "$(slurp)" "/home/zekea/Vault/Uni/Year $year/Semester $semester/$subject/Pictures/$filename.jpg"
 echo "Screenshot saved to ~/Vault/Uni/Year $year/Semester $semester/$subject/Pictures/$filename.jpg"
 echo !\[$filename\]\(Pictures/$filename.jpg\) >>/tmp/notescopy.txt
