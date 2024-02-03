19faec88a2c156119962a757b79c4f476eec6afa
Zeke Aylin <zeke@aylin.net>
2024-02-03 12:46:28 +0000

    Update .config/eww/bar/scripts/brightness-update.sh Update .config/eww/control/config/variables.yuck Update .config/eww/control/config/widgets.yuck Update .config/eww/control/scripts/buttons Update .config/hypr/hyprland.conf Change attributes of .config/scripts/touchpad-toggle.sh

--- dot_config/eww/bar/scripts/executable_brightness-update.sh
+++ dot_config/eww/bar/scripts/executable_brightness-update.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-#
+
+
+if [[ "$1" == "--down" ]]; then
+/usr/bin/eww -c /home/zekea/.config/eww/bar update br_reveal="true"
+brightnessctl set 5%-
+OLD_SCRIPT_PID=$(cat /home/zekea/.cache/eww_bright_update_script)
+kill $OLD_SCRIPT_PID
+echo $$ > /home/zekea/.cache/eww_bright_update_script
+sleep 1
+/usr/bin/eww -c /home/zekea/.config/eww/bar update br_reveal="false"
+elif [[ "$1" == "--up" ]]; then
+/usr/bin/eww -c /home/zekea/.config/eww/bar update br_reveal="true"
+brightnessctl set +5%
+OLD_SCRIPT_PID=$(cat /home/zekea/.cache/eww_bright_update_script)
+kill $OLD_SCRIPT_PID
+echo $$ > /home/zekea/.cache/eww_bright_update_script
+sleep 1
+/usr/bin/eww -c /home/zekea/.config/eww/bar update br_reveal="false"
+fi
