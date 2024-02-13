60f3df3299a2f373edcf9763f1cf61795e4a300d
Zeke Aylin <zeke@aylin.net>
2024-02-13 21:09:30 +0000

    Add .config/scripts/hyprgamemode-lol.sh

---
+++ dot_config/scripts/executable_hyprgamemode-lol.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    xrandr --output DP-2 --primary
+    pkill -f swayidle
+    champ-r
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:allow_tearing true;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
+    exit
+fi
+hyprctl reload
+pkill -f champ-r
+swayidle -w timeout 1800 '/home/zekea/.config/scripts/swaylock.sh' &
