da6f61825c87b078f2a634a1bd610a305a6a0ebc
Zeke Aylin <zeke@aylin.net>
2024-02-13 21:04:14 +0000

    Update .config/eww/control/config/widgets.yuck Update .config/hypr/hyprland.conf Add .config/scripts/hyprgamemode-lol.sh

---
+++ dot_config/scripts/hyprgamemode-lol.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    xrandr --output DP-2 --primary
+    pkill -f swayidle
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:allow_tearing true;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
+    exit
+fi
+hyprctl reload
+swayidle -w timeout 1800 '/home/zekea/.config/scripts/swaylock.sh' &
