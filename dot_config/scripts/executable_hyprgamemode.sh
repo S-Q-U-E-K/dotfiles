e0070991918fbd66e2a1aa601028f0d54c2f2e6f
Zeke Aylin <zeke@aylin.net>
2024-03-04 08:25:20 +0000

    misc

--- dot_config/scripts/executable_hyprgamemode.sh
+++ dot_config/scripts/executable_hyprgamemode.sh
@@ -2,7 +2,7 @@
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     xrandr --output DP-2 --primary
-    pkill -f swayidle
+    pkill -f hypridle
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
@@ -15,4 +15,4 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
     exit
 fi
 hyprctl reload
-swayidle -w timeout 1800 '/home/zekea/.config/scripts/swaylock.sh' &
+hypridle &
